ce6a73196b72b52f88ff75f810834d41ddf18143
Nathan Simpson <nsimpson@turing.ac.uk>
2024-03-26 13:49:06 +0000

    update versioning to rely on pyproject.toml

--- project_template/src/{{python_name}}/__init__.py
+++ project_template/src/{{python_name}}/__init__.py
@@ -2,6 +2,7 @@
 {{ project_name }}: {{ project_short_description }}
 """
 from __future__ import annotations
+from importlib.metadata import version
 
 __all__ = ("__version__",)
-__version__ = "0.1.0"
+__version__ = version(__name__) 
